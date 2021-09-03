ecd615ee678ccbfaa5e613f2e1a4487761342210
David <destrada2193@gmail.com>
2021-09-03 16:01:01 -0700

    Add ExpensesFilter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,20 @@
+import React, { useState } from "react";
+
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2021'); // we can have 2-way-binding ensuring this year, 2021 is the default year displayed in the dropdown title
+                                                            // this is passed in ExpensesFilter's <select> opening tag as the value attribute's value!
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected= {filteredYear} onChangeFilter={filterChangeHandler}/>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
